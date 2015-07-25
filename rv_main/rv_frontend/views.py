fe44661d87ed64a96cb9db7e536b73eae3298938
aruchames <ruchames@princeton.edu>
2015-07-25 04:46:01 +0000

    Added templates, urls, views

--- rv_main/rv_frontend/views.py
+++ rv_main/rv_frontend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
+
+def index(request):
+    t = loader.get_template('index.html')
+    c = RequestContext(request, {})
+    return HttpResponse(t.render(c))
--- rv_main/rv_main/wsgi.py
+++ rv_main/rv_main/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/home/ruchames/replayvalue/rv_main/rv_web')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rv_main.settings")
 
 from django.core.wsgi import get_wsgi_application
