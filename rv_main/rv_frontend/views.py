49a1941223da26cce323a6addf98b4cd7e623932
aruchames <ruchames@princeton.edu>
2015-07-26 04:57:00 +0000

    Started login view and template

--- rv_main/rv_frontend/views.py
+++ rv_main/rv_frontend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -8,3 +10,26 @@ def index(request):
     t = loader.get_template('index.html')
     c = RequestContext(request, {})
     return HttpResponse(t.render(c))
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            t = loader.get_template('profile.html')
+            c = RequestContext(request, {})
+            return HttpResponse(t.render(c))
+        else:
+            t = loader.get_template('banned.html')
+            c = RequestContext(request, {})
+            return HttpResponse(t.render(c))
+    else:
+        t = loader.get_template('login_error.html')
+        c = RequestContext(request, {})
+        return HttpResponse(t.render(c))
+
+
+                           
+
