096b2cc8e3b0a3d6290b9580124bb08533266072
Dean Robinson <dean@ip-172-31-19-87.us-west-2.compute.internal>
2015-08-28 02:01:26 +0000

    login/registration/notarepeat

--- rv_main/rv_frontend/views.py
+++ rv_main/rv_frontend/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
+import re
 
 # Create your views here.
 
@@ -11,30 +14,67 @@ def index(request):
     c = RequestContext(request, {})
     return HttpResponse(t.render(c))
 
-def login_load(request):
-    t = loader.get_template('login.html')
-    c = RequestContext(request, {})
-    return HttpResponse(t.render(c))
-
-def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            t = loader.get_template('profile.html')
-            c = RequestContext(request, {})
-            return HttpResponse(t.render(c))
+def login(request):
+    
+    if request.method == "POST":
+    
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print "FUK YES BITCCHES"
+            if user.is_active:
+                auth_login(request, user)
+                t = loader.get_template('index.html')
+                c = RequestContext(request, {})
+                return HttpResponse(t.render(c))
+            else:
+                t = loader.get_template('banned.html')
+                c = RequestContext(request, {})
+                return HttpResponse(t.render(c))
         else:
-            t = loader.get_template('banned.html')
+            print "oh no"
+            t = loader.get_template('login_error.html')
             c = RequestContext(request, {})
             return HttpResponse(t.render(c))
     else:
-        t = loader.get_template('login_error.html')
+        
+        t = loader.get_template('login.html')
         c = RequestContext(request, {})
         return HttpResponse(t.render(c))
 
-
-                           
-
+def register(request):
+    
+    if request.method == "POST":
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        emailpattern = '[^@]+@[^@]+\.[^@]+'
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        
+        
+        if User.objects.filter(username=username).exists():
+            print "username"
+            messages.add_message(request, messages.ERROR, "Username already exists.")
+            return redirect("register")
+        if User.objects.filter(email=email).exists():
+            print "email"
+            messages.add_message(request, messages.ERROR, "Email address already associated with account.")
+            return redirect("register")
+        if re.match(emailpattern, email) is None:
+            print "FUCK ME HARDER"
+            messages.add_message(request, messages.ERROR, "Bro, do you even email?")
+            return redirect("register")
+    
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        return redirect("index")
+                
+    else:
+        # yay
+        t = loader.get_template('register.html')
+        c = RequestContext(request, {})
+        return HttpResponse(t.render(c))
