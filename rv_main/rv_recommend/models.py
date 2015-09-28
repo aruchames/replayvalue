6b869d93872f6a872eec69ab8e393b585956e123
aruchames <ruchames@princeton.edu>
2015-09-28 19:32:08 +0000

    I don't know what's going on

--- rv_main/rv_recommend/models.py
+++ rv_main/rv_recommend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+"""
+The Game model will contain basic information about the game, title, a list 
+of applicable genres, year released and metacritic score.
+"""
+
+class Game:
+    pass
+
+"""
+The Genre model will contain basic information about different genres. The name
+of that genre, the list of other genres that have been linked to it and the
+
+"""
+
+class Genre:
+
+class TasteVector:
